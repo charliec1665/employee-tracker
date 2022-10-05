11a850fb8f962414e0f4e9ccae69605abfe0c681
Charlie Carter <charliec1665@gmail.com>
2022-10-05 15:14:39 -0400

    department POST method

--- routes/apiRoutes/departmentRoutes.js
+++ routes/apiRoutes/departmentRoutes.js
@@ -17,4 +17,42 @@ router.get('/departments', (req, res) => {
     });
 });
 
+// GET a single department
+router.get('/department/:id', (req, res) => {
+    const sql = `SELECT * FROM departments WHERE id = ?`
+    const params = [req.params.id];
+
+    db.query(sql, params, (err, row) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: row
+        });
+    });
+});
+
+// Create a department via POST method
+router.post('/department', ({ body }, res) => {
+    const sql = `INSERT INTO departments (name)
+                VALUES (?)`;
+    const params = [body.name];
+
+    db.query(sql, params, (err, result) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: body
+        });
+    });
+});
+
+// DELETE a department
+
+
 module.exports = router;
